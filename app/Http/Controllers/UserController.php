c3b87181fb550eb0d5ff5bf8cc347cc45b1f79d5
Santiago Silvestre <silvestre.santiago732@gmail.com>
2023-04-20 19:44:44 -0300

    Add filtro na pesquisa de user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,17 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
 
-        $users = User::get();
+        //$users = User::where('name', 'LIKE', "%{$request->search}%")->get();
+        $search = $request->search;
+        $users = User::where( function ($query) use($search) {
+                if ($search) {
+                    $query->where('email', $search);
+                    $query->orWhere('name', 'LIKE', "%{$search}%");
+                } 
+            } 
+        )->get();
         
         return view('users.index', compact('users'));
     }
@@ -54,15 +62,32 @@ class UserController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(Request $request, $id) {
+    public function update(StoreUpdateUserFormRequest $request, $id) {
 
 
 
         if(!$user = User::find($id))
             return redirect()->route('users.index');
 
-        dd($request->all());
-        // return view('users.edit', compact('user'));
+        $dataForm = $request->only('name', 'email');
+        if ($request->password) 
+            $dataForm['password'] = bcrypt($request->password);
+        
+        
+        $user->update($dataForm);
+        
+        return redirect()->route('users.index');
+    }
+
+    public function destroy($id) {
+
+        //$user = User::where('id', $id)->first();
+        if(!$user = User::find($id))
+            return redirect()->route('users.index');
+
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 
 }
